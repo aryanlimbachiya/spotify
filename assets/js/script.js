7f5806f254105ae6bc771d54f9b5c58660575303
aryanlimbachiya <alimbachiya960@gmail.com>
2025-07-26 18:46:55 +0530

    spotify2

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,14 +38,14 @@ async function getSongs(folder) {
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     songUL.innerHTML = ""
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="assets/music.svg" alt="">
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="assets/img/music.svg" alt="">
                             <div class="info">
                                 <div> ${song.replaceAll("%20", " ")}</div>
                                 <div>Harry</div>
                             </div>
                             <div class="playnow">
                                 <span>Play Now</span>
-                                <img class="invert" src="assets/play.svg" alt="">
+                                <img class="invert" src="assets/img/play.svg" alt="">
                             </div> </li>`;
     }
 
@@ -64,7 +64,7 @@ const playMusic = (track, pause = false) => {
     currentSong.src = `/${currFolder}/` + track
     if (!pause) {
         currentSong.play()
-        play.src = "assets/pause.svg"
+        play.src = "assets/img/pause.svg"
     }
     document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
@@ -128,11 +128,11 @@ async function main() {
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play()
-            play.src = "assets/pause.svg"
+            play.src = "assets/img/pause.svg"
         }
         else {
             currentSong.pause()
-            play.src = "assets/play.svg"
+            play.src = "assets/img/play.svg"
         }
     })
 
